76bea99c9b142a58a78f84e7202543290192c9ad
katapios <fresh_idea@inbox.ru>
2018-03-11 20:45:16 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '558506472:AAG-_Bkh-9zVUxk-DIVrvSjYiNoWYaWGkY8'
+
+const bot = new TelegramBot(TOKEN, {polling; true})
+
+bot.on('message',msg => {
+    bot.sendMessage(msg.chat.id, 'Добрый день, ${msg.from.first_name}')
+})
