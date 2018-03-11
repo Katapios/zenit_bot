6d8b4222a5ecc569bdcf717880233200993378bb
katapios <fresh_idea@inbox.ru>
2018-03-11 21:52:10 +0300

    init5

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '558506472:AAG-_Bkh-9zVUxk-DIVrvSjYiNoWYaWGkY8'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id,  "HI, ${msg.from.first_name}")
+    bot.sendMessage(msg.chat.id,  `Добрый день, $\{msg.from.first_name}`)
 })
