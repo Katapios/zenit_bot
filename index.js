10b8d64c2aefffcc9f915e22f18dbc9b31ba02cb
katapios <fresh_idea@inbox.ru>
2018-03-11 21:45:18 +0300

    init4

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '558506472:AAG-_Bkh-9zVUxk-DIVrvSjYiNoWYaWGkY8'
 
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, ' HI, ${msg.from.first_name}')
+    bot.sendMessage(msg.chat.id,  "HI, ${msg.from.first_name}")
 })
